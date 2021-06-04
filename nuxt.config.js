6de7c846ba9cb1e4e92d9ba4bafa6e2f0f129e60
かる <karuhi.dev@gmail.com>
2021-06-05 02:50:00 +0900

    OGP関係の調整

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,13 +5,58 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    htmlAttrs: {
+      prefix: 'og: http://ogp.me/ns#',
+      lang: 'ja',
+    },
     title: 'Intel World Open Tokyo 特集｜ロケットリーグ 日本コミュニティ',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          '大会情報、オフラインイベントなどや、アップデート、初心者向けTipsなどなどロケットリーグについての様々な情報を発信していくコミュニティWebサイトです。',
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'Intel World Open Tokyo 特集｜ロケットリーグ 日本コミュニティ',
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://iwo-tokyo.rl-japan.com',
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Intel World Open Tokyo 特集｜ロケットリーグ 日本コミュニティ',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          '大会情報、オフラインイベントなどや、アップデート、初心者向けTipsなどなどロケットリーグについての様々な情報を発信していくコミュニティWebサイトです。',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://iwo-tokyo.rl-japan.com/images/ogp.png',
+      },
+
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@RL_Japan' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: 'https://iwo-tokyo.rl-japan.com/favicon.png',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
